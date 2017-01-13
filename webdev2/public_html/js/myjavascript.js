29a72517b16321324451dcb9ff2968424e1006ca
suchin07 <suchin@wp.pl>
2017-01-13 17:27:17 +0100

    ostateczne zmiany

--- webdev2/public_html/js/myjavascript.js
+++ webdev2/public_html/js/myjavascript.js
@@ -1,5 +1,3 @@
-
-
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
--- webdev2/public_html/js/myjquery.js
+++ webdev2/public_html/js/myjquery.js
@@ -1,26 +1,22 @@
 $(document).ready(function(){
     $('#gallery-button').click(function(e){
         e.preventDefault();
-        $('#gallery-extra').slideToggle(1500);
-        $('#gallery-button').text('VIEW LESS');
+        if ($('#gallery-extra').is(':hidden')) {
+            $('#gallery-extra').slideDown(1500);
+            $('#gallery-button').text('VIEW LESS');
+        } else if ($('#gallery-extra').is(':visible')) {
+            $('#gallery-extra').slideUp(1500);
+            $('#gallery-button').text('VIEW MORE');
+        }
     });
-    //end of gallery-button
     $('#blog-button').click(function(e){
         e.preventDefault();
-        $('#blog-extra').slideToggle(1500);
-        $('#blog-button').text('VIEW LESS');
+        if ($('#blog-extra').is(':hidden')) {
+            $('#blog-extra').slideDown(1500);
+            $('#blog-button').text('VIEW LESS');
+        } else if ($('#blog-extra').is(':visible')) {
+            $('#blog-extra').slideUp(1500);
+            $('#blog-button').text('VIEW MORE');
+        }
     });
-    //end of blog-button
 });
-/*$(document).ready(function(){
-    $('#gallery-button').click(function(e){
-        e.preventDefault();
-        $('#gallery-extra').slideDown(1500);
-    });
-    $('#gallery-button').click(function(e){
-        e.preventDefault();
-        $('#gallery-extra').slideUp(1500);
-    });
-    //end of #gallery-button
-    
-});*/
